ce308b7d561c74bb2e5f4a410ee327df4f6cb54c
Queyenth <queyenth@gmail.com>
2014-09-14 00:59:41 +0700

    Changed battery status

--- bspwm/bspanel.sh
+++ bspwm/bspanel.sh
@@ -29,44 +29,7 @@ function statusbar {
 
   function batt() {
     bat=$(cat /sys/class/power_supply/BAT0/capacity)
-    s="«"
-    bar=""
-    case $bat in
-      100)
-        bar=""
-        ;;
-      [0-5])
-        bar="\f8$s$s$s$s$s$s$s$s$s"
-        ;;
-      [5-9])
-        bar="\f3$s\f8$s$s$s$s$s$s$s$s"
-        ;;
-      [1-2]*)
-        bar="$s$s\f8$s$s$s$s$s$s$s"
-        ;;
-      3*)
-        bar="$s$s$s\f8$s$s$s$s$s$s"
-        ;;
-      4*)
-        bar="$s$s$s$s\f8$s$s$s$s$s"
-        ;;
-      5*)
-        bar="$s$s$s$s$s\f8$s$s$s$s"
-        ;;
-      6*)
-        bar="$s$s$s$s$s$s\f8$s$s$s"
-        ;;
-      7*)
-        bar="$s$s$s$s$s$s$s\f8$s$s"
-        ;;
-      8*)
-        bar="$s$s$s$s$s$s$s$s\f8$s"
-        ;;
-      *)
-        bar="$s$s$s$s$s$s$s$s$s"
-        ;;
-    esac
-    echo "$bar"
+    echo $bar
   }
 
   function ncc() {
